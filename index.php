c02aa0c5b540b181382c1dd969099227cc9d831b
Charles Reyes <charleskylereyes16@gmail.com>
2021-02-04 21:03:02 +0800

    Added database and images

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<!-- <img src='./images/albert_einstein.jpg'> -->
+		<?php
+		// names, images, age, birthday, and contact number
+			$age = array(
+				array('Barack Obama', './barack_obama.jpg', 59, '04/08/1961', '09117311384'),
+				array('Ellen DeGeneres', './ellen_degeneres.jpg', 63, '26/01/1958', '09245094743'),
+				array('Johnny Depp', './johnny_depp.jpg', 57, '09/06/1963', '09917098018'),
+				array('Leonardo DiCaprio', './leonardo_dicaprio.jpg', 46, '11/11/1974', '09514808435'),
+				array('Oprah Winfrey', './oprah_winfrey.jpg', 67, '29/01/1954', '09910562466'), 
+				array('Queen Elizabeth', './queen_elizabeth.jpg', 94, '21/04/1926', '09633622496'), 
+				array('Robert Downey Jr.', './robert_downey.jpg', 55, '04/04/1965', '09412095632'), 
+				array('Rowan Atkinson', './rowan_atkinson', 66, '06/01/1955', '09290334376'), 
+				array('Taylor Swift', './taylor_swift.jpg', 31, '13/12/1989', '09029679884'), 
+				array('Meryl Streep', './meryl_streep.jpg', 71, '22/06/1949', '09372167088'), 
+			);
+			print_r($age);
+		?>
+	</body>
+</html>	
